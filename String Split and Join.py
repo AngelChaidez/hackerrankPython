a3cdb2d0edd22dc7a443ae5979b2165f820399b2
Angel Chaidez <angelomarchaidez@gmail.com>
2023-03-29 10:57:00 -0700

    ifelse stringsplit

---
+++ String Split and Join.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3.11
+
+def split_and_join(line):
+    # write your code here
+    line=line.split(" ")
+    line="-".join(line)
+    return line
+
+if __name__ == '__main__':
+    line = input()
+    result = split_and_join(line)
+    print(result)
